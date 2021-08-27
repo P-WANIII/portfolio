8c21ec7d36c135f0e50107fce42000a4a066bc93
Pranavi Wani <pranaviwani2000@gmail.com>
2021-08-27 22:11:22 +0530

    2nd commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+
+
+// about section tabs
+	
+(() =>{
+	const aboutSection = document.querySelector(".about-section"),
+	tabsContainer = document.querySelector(" .about-tabs");
+
+	tabsContainer.addEventListener("click", (event) =>{
+		if(event.target.classList.contains("tab-item")&&
+			!event.target.classList.contains("active")){
+				const target = event.target.getAttribute("data-target");
+				// deactive exsisting active 'tab-items'
+				tabsContainer.querySelector(".active").classList.remove("outer-shadow","active");
+				// active new 'tab items'
+				event.target.classList.add("active","outer-shadow");
+				//deactivate existing active 'tab-content'
+				aboutSection.querySelector(".tab-content.active").classList.remove("active");
+				//activate new "tab-content"
+				aboutSection.querySelector(target).classList.add("active");
+			}
+	})
+
+})();
+
